e23a7372e3391d024fb7947cf21c102de66f5418
Raj Dadia <135305625+rajdadia998@users.noreply.github.com>
2023-11-08 10:02:16 +0530

    Add files via upload

---
+++ main.dart
@@ -0,0 +1,58 @@
+import 'package:location/loc.dart';
+import 'package:location/main.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Splash Screen',
+      home: Splash(),
+    );
+  }
+}
+
+class Splash extends StatefulWidget {
+  const Splash({super.key});
+
+  @override
+  State<Splash> createState() => _SplashState();
+}
+
+class _SplashState extends State<Splash> {
+  
+  @override
+  void initState() {
+    super.initState();
+    Future.delayed(const Duration(seconds: 3),(){
+      Navigator.pushReplacement(context, MaterialPageRoute(builder: (context)=> Homepage()));
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            
+            Image.asset('assets/loc.png',height: 270,),  
+          ],
+        ),
+      ),
+    );
+  }
+}
+
